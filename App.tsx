74063ba9164cc74b9a9e0970caeb1ae950cb3d07
Davod <davodnikoyii@gmail.com>
2024-09-30 11:05:03 +0200

    app file updated

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <Text>Home</Text>
       <StatusBar style="auto" />
     </View>
   );
